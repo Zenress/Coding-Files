dfee0902ecf9ac3de00cb6ee610e7abf3af65d00
Zenress <Zenress@users.noreply.github.com>
2020-11-05 10:56:53 +0100

    Update

---
+++ repos/EsportDanmark PEO/EsportDanmark PEO/ViewModel/ViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EsportDanmark_PEO.ViewModel
+{
+    class ViewModel
+    {
+    }
+
+    public class MainWindowViewModel
+    {
+
+    }
+
+    public class PlayerDataViewModel
+    {
+
+    }
+}
