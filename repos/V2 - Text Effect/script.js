235edbb4eef80649e57c014321d245487d0d27de
Zenress <Zenress@users.noreply.github.com>
2021-02-23 09:27:18 +0100

    ss

--- repos/V2 - Text Effect/script.js
+++ repos/V2 - Text Effect/script.js
@@ -1,4 +1,5 @@
 var fruits = ['Æble','Citron','Pære','Banan'];
+var fruitColor = ['Red','Yellow','Green','Blue']
 var select = document.querySelector('p.text');
 
 // Function that types a randomly selected item from fruits to a string
@@ -6,34 +7,29 @@ function typeSentence()
 {
     var randomNr = Math.floor(Math.random() * fruits.length)
     let split = fruits[randomNr].split('');
-    console.log(fruits[1]);
-    console.log(split[0]);
     for (let index = 0; index < split.length; index++) 
     {
         setTimeout(() => {select.append(split[index]);},index * 650);
     }
+    select.style.color = fruitColor[randomNr];
 }
 
 // Function for deleting the sentence that typeSentence forms
 function deleteSentence() 
-{
-    console.log(select.innerHTML);
+{    
     let split = select.innerHTML.split("");
-    console.log(split.length);
     let nr = split.length;
     for (let index = 0; index < split.length; index++) 
     {
         setTimeout(() => {select.innerHTML = select.innerHTML.substring(0,nr  - 1);},index * 650);
         setTimeout(() => {nr--;},index * 650);
-    }
-    
+    }    
 }
 
 // A function that makes typeSentence and deleteSentence work together in harmony
 function typeEffect() 
 {
     typeSentence();
-
     setTimeout(() => {deleteSentence()},5000)
 }
 
